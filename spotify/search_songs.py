2c7b01b5c43d42929d2e4a403d01cdd013a0b2c0
Niels Demeyer <73473786+niels-demeyer@users.noreply.github.com>
2023-11-28 14:36:35 +0100

    Update search_songs.py

--- spotify/search_songs.py
+++ spotify/search_songs.py
@@ -1,25 +1,31 @@
 import base64
 import json
 from requests import post, get
-import sys
 import os
+from dotenv import load_dotenv
 import psycopg2
-import urllib.parse
 import time
 from fuzzywuzzy import fuzz
+from typing import List, Optional, Dict, Any
+import pprint
 
+env_path = "your path to .env file"
+load_dotenv(env_path)
 
-# Adding the parent directory to the system path to access the config module
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
-from config import config
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 conn = psycopg2.connect(
-    dbname=config.DB_NAME,
-    user=config.DB_USER,
-    password=config.DB_PASSWORD,
-    host=config.DB_HOST,
-    port=config.DB_PORT,
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT,
 )
 
 print("Connected to database")
@@ -66,7 +72,7 @@ def get_token():
     try:
         print("Attempting to get Spotify token...")
 
-        auth_string = config.CLIENT_ID + ":" + config.CLIENT_SECRET
+        auth_string = CLIENT_ID + ":" + CLIENT_SECRET
         auth_bytes = auth_string.encode("utf-8")
         auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
@@ -96,6 +102,126 @@ def get_token():
 
     except Exception as e:
         print(f"Error in getting token: {e}")
+
+
+def create_spotify_data_table():
+    try:
+        print("Attempting to create spotify_data_songs table...")
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS spotify_data_songs (
+            id SERIAL PRIMARY KEY,
+            genre VARCHAR(255),
+            artist VARCHAR(255),
+            song VARCHAR(255),
+            album VARCHAR(255),
+            artist_id VARCHAR(255),
+            song_id VARCHAR(255),
+            album_id VARCHAR(255),
+            UNIQUE(genre, artist, song)
+        );
+        """
+        cur.execute(create_table_query)
+        conn.commit()
+        print("Table spotify_data_songs ensured to exist successfully")
+    except Exception as e:
+        print(f"Error in creating table: {e}")
+
+
+def ensure_spotify_data_table_exists():
+    try:
+        print("Ensuring spotify_data_songs table exists...")
+        cur.execute(
+            """
+            SELECT EXISTS (
+                SELECT FROM information_schema.tables 
+                WHERE table_name = 'spotify_data_songs'
+            );
+            """
+        )
+        result = cur.fetchone()
+        if result[0]:
+            print("Table spotify_data_songs exists")
+            return True
+        else:
+            create_spotify_data_table()
+    except Exception as e:
+        print(f"Error in ensuring table exists: {e}")
+
+
+def get_genre_music(cur):
+    try:
+        cur.execute(
+            """
+            SELECT genre, artist, project_title, track
+            FROM all_results
+            ORDER BY genre, artist, project_title, track
+            """
+        )
+        genre_music = cur.fetchall()
+        return genre_music
+    except Exception as e:
+        print(f"Error in getting genre music: {e}")
+
+
+def get_spotify_data_songs(cur):
+    try:
+        cur.execute(
+            """
+            SELECT genre, artist, album, song
+            FROM spotify_data_songs
+            ORDER BY genre, artist, album, song
+            """
+        )
+        spotify_data_songs = cur.fetchall()
+        return spotify_data_songs
+    except Exception as e:
+        print(f"Error in getting spotify data songs: {e}")
+
+
+def get_not_results(cur):
+    try:
+        cur.execute(
+            """
+            SELECT genre, artist, song
+            FROM not_results
+            ORDER BY genre, artist, song
+            """
+        )
+        not_results = cur.fetchall()
+        return not_results
+    except Exception as e:
+        print(f"Error in getting not results: {e}")
+
+
+def get_unique_genre_music(cur, genres):
+    # Existing function calls
+    genre_music_list = get_genre_music(cur)
+    spotify_data_songs = get_spotify_data_songs(cur)
+    not_results_list = get_not_results(cur)
+
+    print(f"Genre music list: {len(genre_music_list)}")
+    print(f"Spotify data songs: {len(spotify_data_songs)}")
+    print(f"Not results list: {len(not_results_list)}")
+
+    # Convert spotify_data_songs to a set of (album, song) for faster lookup
+    spotify_data_songs_set = set((song[2], song[3]) for song in spotify_data_songs)
+    not_results_set = set((song[1], song[2]) for song in not_results_list)
+
+    unique_genre_music = []
+    unique_not_results = []
+
+    # Check each item in genre_music_list for unique (album, song) combination
+    for song in genre_music_list:
+        album_song_pair = (song[2], song[3])
+        if album_song_pair not in spotify_data_songs_set:
+            unique_genre_music.append(song)
+
+    for song in unique_genre_music:
+        album_song_pair = (song[1], song[3])
+        if album_song_pair not in not_results_set:
+            unique_not_results.append(song)
+
+    return unique_not_results
 
 
 def search_spotify(item_type, query, artist_name, headers):
@@ -145,27 +271,62 @@ def search_spotify(item_type, query, artist_name, headers):
     )
 
 
-def create_spotify_data_table():
+def create_not_results_table(cur, conn):
     try:
-        print("Attempting to create spotify_data_songs table...")
+        print("Attempting to create not_results table...")
         create_table_query = """
-        CREATE TABLE IF NOT EXISTS spotify_data_songs (
+        CREATE TABLE IF NOT EXISTS not_results (
             id SERIAL PRIMARY KEY,
             genre VARCHAR(255),
             artist VARCHAR(255),
             song VARCHAR(255),
-            album VARCHAR(255),
-            artist_id VARCHAR(255),
-            song_id VARCHAR(255),
-            album_id VARCHAR(255),
             UNIQUE(genre, artist, song)
         );
         """
         cur.execute(create_table_query)
         conn.commit()
-        print("Table spotify_data_songs ensured to exist successfully")
+        print("Table not_results ensured to exist successfully")
     except Exception as e:
         print(f"Error in creating table: {e}")
+
+
+def check_table_exists(cur, table_name):
+    cur.execute(
+        f"""
+        SELECT EXISTS (
+            SELECT FROM information_schema.tables 
+            WHERE  table_name = '{table_name}'
+        );
+    """
+    )
+    return cur.fetchone()[0]
+
+
+def not_results(genre, artist, song):
+    try:
+        if not check_table_exists(cur, "not_results"):
+            create_not_results_table(cur, conn)  # Ensure table exists
+        cur.execute(
+            """
+            INSERT INTO not_results (genre, artist, song)
+            VALUES (%s, %s, %s)
+            ON CONFLICT (genre, artist, song) DO NOTHING;
+            """,
+            (
+                genre,
+                artist,
+                song,
+            ),
+        )
+        conn.commit()
+        print(
+            f'Successfully stored song "{song}" by artist "{artist}" into the not_results TABLE.'
+        )
+    except psycopg2.IntegrityError:
+        conn.rollback()
+        print(f"Duplicate entry detected: {genre}, {artist}, {song}. Skipping.")
+    except Exception as e:
+        print(f"Error in storing not results: {e}")
 
 
 def search_and_store_spotify_data(genre, artist, song, headers, album):
@@ -177,6 +338,8 @@ def search_and_store_spotify_data(genre, artist, song, headers, album):
 
     if not results:
         print(f"No match found for song '{song}' by artist '{artist}'.")
+        not_results(genre, artist, song)
+        print(f'Storing song "{song}" by artist "{artist}" into the not_results.')
         return
 
     best_match = find_best_match(artist, song, album, results)
@@ -219,7 +382,7 @@ def search_and_store_spotify_data(genre, artist, song, headers, album):
             )
             conn.commit()
             print(
-                f"Successfully stored data for song '{song}' by artist '{artist}' into the database."
+                f"Successfully stored data for song '{song}' by artist '{artist}' into the database spotify_data_songs."
             )
         except psycopg2.IntegrityError:
             conn.rollback()
@@ -228,130 +391,98 @@ def search_and_store_spotify_data(genre, artist, song, headers, album):
         print(f"No matching artist-song combination found for {artist} - {song}")
 
 
-def get_last_processed_artist_song(genre):
-    cur.execute(
-        """
-        SELECT artist, song
-        FROM spotify_data_songs  
-        WHERE genre = %s 
-        ORDER BY id DESC 
-        LIMIT 1;
-    """,
-        (genre,),
+def calculate_similarity(source: str, target: str) -> int:
+    """Calculate the similarity score between two strings."""
+    return fuzz.ratio(source, target)
+
+
+def validate_result_structure(result: Dict[str, Any]) -> bool:
+    """Check if the result structure is as expected."""
+    return (
+        isinstance(result, dict)
+        and "artists" in result
+        and isinstance(result["artists"], list)
+        and result["artists"]
     )
-    result = cur.fetchone()
-    if result:
-        return result
-    return None, None
 
 
-def find_best_match(artist, song, album, results):
+def find_best_match(
+    artist: str,
+    song: str,
+    album: Optional[str],
+    results: List[Dict[str, Any]],
+    artist_threshold: int = 25,
+    song_threshold: int = 25,
+    debug: bool = False,
+) -> Optional[Dict[str, Any]]:
+    """
+    Find the best match for a song from a list of results based on artist, song, and optionally album.
+    """
     best_match = None
     best_score = -1
 
     for idx, result in enumerate(results):
-        # Ensure the structure of the result is as expected
-        if not (
-            isinstance(result, dict)
-            and "artists" in result
-            and isinstance(result["artists"], list)
-            and result["artists"]
-        ):
-            print(
-                f"DEBUG - Skipping result {idx} due to unexpected structure. Actual structure: {result}"
-            )
+        if not validate_result_structure(result):
+            if debug:
+                print(f"DEBUG - Skipping result {idx} due to unexpected structure.")
             continue
 
-        artist_similarity = fuzz.ratio(artist, result["artists"][0]["name"])
-        song_similarity = fuzz.ratio(song, result["name"])
+        artist_similarity = calculate_similarity(artist, result["artists"][0]["name"])
+        song_similarity = calculate_similarity(song, result["name"])
 
-        # print(f"DEBUG - Result {idx} artist similarity: {artist_similarity}")
-        # print(f"DEBUG - Result {idx} song similarity: {song_similarity}")
+        if debug:
+            print(f"DEBUG - Result {idx} artist similarity: {artist_similarity}")
+            print(f"DEBUG - Result {idx} song similarity: {song_similarity}")
 
-        if artist_similarity > 80 and song_similarity > 80:
+        if artist_similarity > artist_threshold and song_similarity > song_threshold:
             current_score = artist_similarity + song_similarity
 
-            if (
-                album
-                and "album" in result
-                and isinstance(result["album"], dict)
-                and result["album"].get("name") == album
-            ):  # Enhanced check
+            if album and "album" in result and result["album"].get("name") == album:
                 current_score += 10
-                # print(f"DEBUG - Result {idx} matched album, boosting score.")
+                if debug:
+                    print(f"DEBUG - Result {idx} matched album, boosting score.")
 
-            print(f"DEBUG - Result {idx} current score: {current_score}")
+            if debug:
+                print(f"DEBUG - Result {idx} current score: {current_score}")
 
             if current_score > best_score:
                 best_score = current_score
                 best_match = result
-        else:
+        elif debug:
             print(f"DEBUG - Result {idx} discarded due to low similarity.")
 
     return best_match
 
 
 def main():
+    ensure_spotify_data_table_exists()
     try:
-        # Ensure the spotify_data_songs table exists before any operations.
-        print("Ensuring spotify_data_songs table exists...")
-        create_spotify_data_table()
-
-        # Fetch the Spotify token.
-        print("Fetching the token from Spotify...")
         token = get_token()
         if not token:
             print("Could not obtain token. Exiting...")
             return
-
         headers = get_auth_headers(token)
-
-        # Process each genre.
-        for genre in genres:
-            print(f"Processing genre: {genre}")
-
-            # Fetch the last processed artist and song for the current genre.
-            last_processed_artist, last_processed_song = get_last_processed_artist_song(
-                genre
-            )
-
-            # Fetch artist and song details from your database.
-            cur.execute(
-                f"""
-                SELECT m.artist, m.project_title, s.song_title 
-                FROM {genre}_music AS m
-                JOIN {genre}_songs AS s ON m.id = s.{genre}_music_id
-                ORDER BY m.artist, s.song_title
-                """
-            )
-
-            artist_song_mappings = cur.fetchall()
-
-            start_processing = False
-            if not last_processed_artist and not last_processed_song:
-                start_processing = (
-                    True  # If there's no last processed item, process all.
-                )
-
-            for mapping in artist_song_mappings:
-                artist, album, song = mapping
-
-                # Start processing from the last processed item.
-                if not start_processing:
-                    if artist == last_processed_artist and song == last_processed_song:
-                        start_processing = True
-                    continue
-
+        get_unique_genre_music_list = get_unique_genre_music(cur, genres)
+        print(f"Unique genre music list: {len(get_unique_genre_music_list)}")
+        for unique in get_unique_genre_music_list:
+            genre = unique[0]
+            artist = unique[1]
+            album = unique[2]
+            song = unique[3]
+            print(f"Genre: {genre}, Artist: {artist}, Album: {album}, Song: {song}")
+            try:
                 search_and_store_spotify_data(genre, artist, song, headers, album)
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                print("Refreshing token...")
+                token = get_token()
+                if not token:
+                    print("Could not obtain token. Exiting...")
+                    return
+                headers = get_auth_headers(token)
 
     except Exception as e:
         print(f"An error occurred: {e}")
-    finally:
-        if cur:
-            cur.close()
-        if conn:
-            conn.close()
-        print("Database connection closed")
 
 
 if __name__ == "__main__":
