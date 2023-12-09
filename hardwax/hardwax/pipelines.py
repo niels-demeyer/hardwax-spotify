e6805fff97f7fdd69515a1804ef7d95eeb39dabd
niels demeyer <niels.demeyer99@gmail.com>
2023-12-09 20:17:44 +0100

    adding the scrapy folder

---
+++ hardwax/hardwax/pipelines.py
@@ -0,0 +1,71 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import psycopg2
+from scrapy.exceptions import DropItem
+from dotenv import load_dotenv
+import os
+
+env_path = r"../../config/.env"
+load_dotenv(dotenv_path=env_path)
+host = os.getenv("DB_HOST")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+dbname = "hardwax"
+
+
+class PostgreSQLPipeline(object):
+    def __init__(self):
+        self.connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            dbname=dbname,  # connect to the scrapy_hardwax database
+        )
+        self.connection.autocommit = True
+        self.cursor = self.connection.cursor()
+
+    def open_spider(self, spider):
+        # create the table
+        self.cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS music_albums (
+                id SERIAL PRIMARY KEY,
+                artist TEXT,
+                album TEXT,
+                label TEXT,
+                label_issue TEXT,
+                genre TEXT,
+                track TEXT,
+                UNIQUE(artist, album, genre, track)
+            )
+            """
+        )
+        self.connection.commit()
+
+    def process_item(self, item, spider):
+        self.cursor.execute(
+            """
+            INSERT INTO music_albums (artist, album, label, label_issue, genre, track) 
+            VALUES (%s, %s, %s, %s, %s, %s)
+            ON CONFLICT (artist, album, genre, track) DO NOTHING
+            """,
+            (
+                item.get("artist"),
+                item.get("album"),
+                item.get("label"),
+                item.get("label_issue"),
+                spider.name,
+                item.get("track"),
+            ),
+        )
+        self.connection.commit()
+        return item
+
+    def close_spider(self, spider):
+        self.cursor.close()
+        self.connection.close()
---
+++ hardwax/hardwax/spiders/reggae.py
@@ -0,0 +1,51 @@
+import scrapy
+
+
+# this has section in url
+class ReggaeSpider(scrapy.Spider):
+    name = "reggae"
+    allowed_domains = ["hardwax.com"]
+    page_number = 1
+
+    def __init__(self, *args, **kwargs):
+        super(ReggaeSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [f"https://hardwax.com/section/{self.name}/?page=1"]
+
+    def parse(self, response):
+        print("URL:", response.url)
+        divs = response.css("div.qv")
+        print("Number of divs found:", len(divs))
+        if divs:
+            data_found = False
+            for li in response.css("li"):
+                div = li.css("div.qv")
+                a_elements = div.css("a")
+                if len(a_elements) >= 2:
+                    label = a_elements[0].attrib["title"]
+                    artist_album = a_elements[1].attrib.get("title", "")
+                    if (
+                        artist_album.count(":") == 1
+                    ):  # Only process the element if title contains exactly one colon
+                        artist, album = artist_album.split(":")
+                        label_issue = a_elements[1].css("::text").get()
+                        for a in li.css("a.sa"):
+                            title = a.attrib.get("title", "")
+                            if (
+                                title.count(":") == 1
+                            ):  # Only process the element if title contains exactly one colon
+                                track = title.split(":")[1]
+                                print("Data:", artist, album, label, label_issue, track)
+                                yield {
+                                    "artist": artist.strip(),
+                                    "album": album.strip(),
+                                    "label": label.strip(),
+                                    "label_issue": label_issue.strip(),
+                                    "track": track.strip(),
+                                }
+                                data_found = True
+            if data_found:
+                self.page_number += 1
+                next_page = (
+                    f"https://hardwax.com/section/{self.name}/?page={self.page_number}"
+                )
+                yield scrapy.Request(next_page, self.parse)
