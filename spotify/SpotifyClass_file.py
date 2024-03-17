6c32db4fb80d4581fcf928a021372f377818318d
Niels Demeyer <niels.demeyer99@gmail.com>
2024-03-17 22:27:02 +0100

    getting acces token works

--- spotify/SpotifyClass_file.py
+++ spotify/SpotifyClass_file.py
@@ -30,8 +30,48 @@ class SpotifyClass:
             host=self.dbhost,
             port=self.dbport,
         )
+        print("Database connected")
         # load the environment variables for the spotify api
         self.client_id = os.getenv("CLIENT_ID")
         self.client_secret = os.getenv("CLIENT_SECRET")
         self.refresh_token = os.getenv("REFRESH_TOKEN")
         self.spotify_username = os.getenv("SPOTIFY_USERNAME")
+        # api varialbes
+        self.token = self.get_access_token()
+        print("Spotify token acquired")
+
+    # Functions to get the access token
+    def get_access_token(self):
+        try:
+            print("Attempting to get Spotify token...")
+
+            auth_string = self.client_id + ":" + self.client_secret
+            auth_bytes = auth_string.encode("utf-8")
+            auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+            url = "https://accounts.spotify.com/api/token"
+            headers = {
+                "Authorization": "Basic " + auth_base64,
+                "Content-Type": "application/x-www-form-urlencoded",
+            }
+            data = {"grant_type": "client_credentials"}
+            result = post(url, headers=headers, data=data)
+
+            if result.status_code != 200:
+                print(f"Failed to get token: {result.status_code} - {result.text}")
+                return None
+
+            try:
+                json_result = json.loads(result.content)
+                print(result.content)
+                token = json_result.get("access_token", None)
+                if not token:
+                    print(f"Access token not found in response: {result.content}")
+                print(f"Token acquired: {token}")
+                return token
+            except json.JSONDecodeError as e:
+                print(f"Failed to decode JSON response: {e}")
+                print(f"Response content: {result.content}")
+
+        except Exception as e:
+            print(f"Error in getting token: {e}")
---
+++ spotify/test.py
@@ -0,0 +1,6 @@
+from SpotifyClass_file import SpotifyClass
+
+spotify = SpotifyClass()
+print(spotify.client_id)
+print(spotify.client_secret)
+print(spotify.refresh_token)
