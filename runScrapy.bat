72ca368f6b07b7b167a7f9b81f1f3e5a69585981
Niels Demeyer <niels.demeyer99@gmail.com>
2024-03-17 21:18:01 +0100

    do all the spiders

--- runScrapy.bat
+++ runScrapy.bat
@@ -1,4 +1,12 @@
 @echo off
 cd /d "hardwax\hardwax\spiders"
-scrapy crawl ambient
+
+for %%A in (*.py) do (
+    if not "%%A"=="__init__.py" (
+        for /f "delims=" %%B in ("%%~nA") do (
+            scrapy crawl %%B
+        )
+    )
+)
+
 pause
