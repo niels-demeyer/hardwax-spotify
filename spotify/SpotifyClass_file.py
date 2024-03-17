2a16be05d141075b7a075ee4b7528763d9c6ce2b
Niels Demeyer <niels.demeyer99@gmail.com>
2024-03-17 22:08:33 +0100

    loaded env variables

--- spotify/SpotifyClass_file.py
+++ spotify/SpotifyClass_file.py
@@ -12,3 +12,19 @@ from typing import List, Optional, Dict, Any
 from requests import post, get, put
 from datetime import datetime
 from psycopg2 import sql
+
+
+class SpotifyClass:
+    def __init__(self):
+        load_dotenv()
+        # load the environment variables for the database
+        self.dbname = os.getenv("DB_NAME")
+        self.dbuser = os.getenv("DB_USER")
+        self.dbpassword = os.getenv("DB_PASSWORD")
+        self.dbhost = os.getenv("DB_HOST")
+        self.dbport = os.getenv("DB_PORT")
+        # load the environment variables for the spotify api
+        self.client_id = os.getenv("CLIENT_ID")
+        self.client_secret = os.getenv("CLIENT_SECRET")
+        self.refresh_token = os.getenv("REFRESH_TOKEN")
+        self.spotify_username = os.getenv("SPOTIFY_USERNAME")
