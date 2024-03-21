3db2599412a524019a28059f38a5d25f1ecd26b1
Niels Demeyer <niels.demeyer99@gmail.com>
2024-03-21 16:19:15 +0100

    removed old api code

--- spotify/SpotifyClass_file.py
+++ spotify/SpotifyClass_file.py
@@ -12,6 +12,8 @@ from typing import List, Optional, Dict, Any
 from requests import post, get, put
 from datetime import datetime
 from psycopg2 import sql
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 
 class SpotifyClass:
@@ -31,65 +33,20 @@ class SpotifyClass:
             port=self.dbport,
         )
         print("Database connected")
-        # load the environment variables for the spotify api
-        self.client_id = os.getenv("CLIENT_ID")
-        self.client_secret = os.getenv("CLIENT_SECRET")
-        self.refresh_token = os.getenv("REFRESH_TOKEN")
-        self.spotify_username = os.getenv("SPOTIFY_USERNAME")
-        # api varialbes
-        self.token = self.get_access_token()
-        print("Spotify token acquired")
-        self.headers = self.get_headers()
-        print("Spotify headers acquired")
+
+        # load the environment variables for the spotipy library
+        self.spotipy_client_id = os.getenv("SPOTIPY_CLIENT_ID")
+        self.spotipy_client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
+        self.auth_manager = SpotifyClientCredentials(
+            client_id=self.spotipy_client_id, client_secret=self.spotipy_client_secret
+        )
+        self.sp = spotipy.Spotify(auth_manager=self.auth_manager)
 
         # Define the variables
         self.spotify_data_songs = None
         self.music_albums_unique = None
         self.music_albums_all = None
         self.spotify_response = []
-
-    # Functions to get the access token
-    def get_access_token(self):
-        try:
-            print("Attempting to get Spotify token...")
-
-            auth_string = self.client_id + ":" + self.client_secret
-            auth_bytes = auth_string.encode("utf-8")
-            auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
-
-            url = "https://accounts.spotify.com/api/token"
-            headers = {
-                "Authorization": "Basic " + auth_base64,
-                "Content-Type": "application/x-www-form-urlencoded",
-            }
-            data = {"grant_type": "client_credentials"}
-            result = post(url, headers=headers, data=data)
-
-            if result.status_code != 200:
-                print(f"Failed to get token: {result.status_code} - {result.text}")
-                return None
-
-            try:
-                json_result = json.loads(result.content)
-                print(result.content)
-                token = json_result.get("access_token", None)
-                if not token:
-                    print(f"Access token not found in response: {result.content}")
-                print(f"Token acquired: {token}")
-                return token
-            except json.JSONDecodeError as e:
-                print(f"Failed to decode JSON response: {e}")
-                print(f"Response content: {result.content}")
-
-        except Exception as e:
-            print(f"Error in getting token: {e}")
-
-    # Function to get the headers
-    def get_headers(self):
-        return {
-            "Authorization": f"Bearer {self.token}",
-            "Content-Type": "application/json",
-        }
 
     def damerau_levenshtein_distance(self, s1, s2):
         """
