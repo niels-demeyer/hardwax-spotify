489a6a07545a12b362cf6e17ef2d7b1fa328412f
Niels Demeyer <niels.demeyer99@gmail.com>
2024-03-29 10:35:12 +0100

    Refactor search_songs.py to handle empty album list

--- spotify/search_songs.py
+++ spotify/search_songs.py
@@ -3,6 +3,8 @@ from SpotifyClass_file import SpotifyClass
 spotify = SpotifyClass()
 
 spotify_data_albums = spotify.select_table_dict("spotify_data_albums")
-print(spotify_data_albums)
-# result = spotify.search_track(spotify_data_albums=spotify_data_albums)
-# spotify.save_to_spotify_data_songs(result)
+if len(spotify_data_albums) == 0:
+    print("No albums found in the database that still need to be searched.")
+    exit()
+result = spotify.search_track(spotify_data_albums=spotify_data_albums)
+spotify.save_to_spotify_data_songs(result)
