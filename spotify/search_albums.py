b7b601c4cc48abb58d6155ffdf43be72f7c7089a
Niels Demeyer <niels.demeyer99@gmail.com>
2024-04-01 22:36:00 +0200

    added comments for search_albums

--- spotify/search_albums.py
+++ spotify/search_albums.py
@@ -1,3 +1,9 @@
+"""
+
+With this script, we can search for albums in the Spotify API. We can search for albums by name, artist. The search results are stored in the 'spotify_data_albums' table.
+
+"""
+
 from SpotifyClass_file import SpotifyClass
 
 spotify = SpotifyClass()
