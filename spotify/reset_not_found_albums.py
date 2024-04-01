338f05d8eb375dbc57d1a190b6e91a6eef972a28
Niels Demeyer <niels.demeyer99@gmail.com>
2024-04-01 22:34:35 +0200

    Reset 'not_found' column in 'spotify_data_albums' table

--- spotify/reset_not_found_albums.py
+++ spotify/reset_not_found_albums.py
@@ -1,3 +1,9 @@
+"""
+
+With this script, we can reset the 'not_found' column in the 'spotify_data_albums' table to 0. This is useful when we want to re-run the script that fetches the data from the Spotify API.
+
+"""
+
 from SpotifyClass_file import SpotifyClass
 
 spotify = SpotifyClass()
